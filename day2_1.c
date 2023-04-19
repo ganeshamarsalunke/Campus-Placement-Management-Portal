de144ecb76d63ae260cbe47eef2eca39637f60d4
shubhamsandhan <shubhamsandhan>
2023-04-19 16:18:29 +0530

    day2_1

---
+++ day2_1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+{
+   // Num ---> valid
+    //num --> valid
+    //1num -->invalid
+    //num_1 --> valid
+    //num 1 -->invalid
+    //num-1 --> invalid
+    //num12 -->valid
+
+    //Num and num they are different
+    return 0;
+}
